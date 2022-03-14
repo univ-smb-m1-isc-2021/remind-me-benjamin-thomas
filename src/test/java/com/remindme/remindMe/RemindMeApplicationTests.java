18506cd70636ca098c50eb2170dafccaa05f5237
Benjamin <tyazze@users.noreply.github.com>
2022-03-14 17:39:03 +0100

    template spring

---
+++ src/test/java/com/remindme/remindMe/RemindMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remindme.remindMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
