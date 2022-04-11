202eb6e67fb554c775c6dcc9784e89e174e15125
Benjamin <tyazze@users.noreply.github.com>
2022-04-11 10:18:02 +0200

    passage de notificationChannels dans settings

--- src/main/js/components/Home.js
+++ src/main/js/components/Home.js
@@ -5,8 +5,7 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            notifications: [],
-            notificationChannels: []
+            notifications: []
         }
     }
 
@@ -16,11 +15,6 @@ class Home extends Component {
             .then(response => response.json())
             .then((data) => {
                 this.setState({notifications: data})
-            });
-        fetch("/api/get_notification_channels")
-            .then(response => response.json())
-            .then((data) => {
-                this.setState({notificationChannels: data})
             });
     }
 
@@ -56,30 +50,6 @@ class Home extends Component {
             });
     }
 
-    deleteNotificationChannel(id){
-        fetch("/api/delete_notification_channel?id="+id)
-            .then(response => response)
-            .then((data) => {
-                console.log(data);
-            });
-        const data = this.state.notificationChannels.filter(i => i.id !== id)
-        this.setState({notificationChannels: data})
-    }
-
-    addNotificationChannel(){
-        var name = document.getElementById("name").value;
-        var type = document.getElementById("type").value;
-        var destination = document.getElementById("destination").value;
-
-        fetch("/api/create_notification_channel?name=" + name + "&type=" + type + "&destination=" + destination)
-            .then(response => response.json())
-            .then((data) => {
-                const tmp = this.state.notificationChannels;
-                tmp.push(data);
-                this.setState({notificationChannels: tmp});
-            });
-    }
-
     render() {
         return (
             <div>
@@ -92,15 +62,6 @@ class Home extends Component {
                     Repetition : <input id="repeat" type="text"/><br/>
                     Frequence (jour) : <input id="frequence" type="number"/><br/>
                     <button onClick={() => this.addNotification()}>Créer</button>
-                </div>
-                <hr/>
-                {this.state.notificationChannels.map(item => <div id={"card_" + item["id"]}><p>Nom : {item["name"]}<br/>Type : {item["type"]}<br/>Destination : {item["destination"]}<br/><button onClick={() => this.deleteNotificationChannel(item["id"])}>Supprimer</button></p></div>)}
-                <hr/>
-                <div>
-                    Nom : <input id="name" type="text"/><br/>
-                    Type : <input id="type" type="text"/><br/>
-                    Destination : <input id="destination" type="text"/><br/>
-                    <button onClick={() => this.addNotificationChannel()}>Créer</button>
                 </div>
             </div>
         );
--- src/main/js/components/Settings.js
+++ src/main/js/components/Settings.js
@@ -5,18 +5,55 @@ class Settings extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            result: []
+            result: [],
+            notificationChannels: []
         }
     }
 
     componentDidMount() {
         const { location } = this.props;
+        fetch("/api/get_notification_channels")
+            .then(response => response.json())
+            .then((data) => {
+                this.setState({notificationChannels: data})
+            });
+    }
+
+    deleteNotificationChannel(id){
+        fetch("/api/delete_notification_channel?id="+id)
+            .then(response => response)
+            .then((data) => {
+                console.log(data);
+            });
+        const data = this.state.notificationChannels.filter(i => i.id !== id)
+        this.setState({notificationChannels: data})
+    }
+
+    addNotificationChannel(){
+        var name = document.getElementById("name").value;
+        var type = document.getElementById("type").value;
+        var destination = document.getElementById("destination").value;
+
+        fetch("/api/create_notification_channel?name=" + name + "&type=" + type + "&destination=" + destination)
+            .then(response => response.json())
+            .then((data) => {
+                const tmp = this.state.notificationChannels;
+                tmp.push(data);
+                this.setState({notificationChannels: tmp});
+            });
     }
 
     render() {
         return (
             <div>
-                Hello Settings
+                {this.state.notificationChannels.map(item => <div id={"card_" + item["id"]}><p>Nom : {item["name"]}<br/>Type : {item["type"]}<br/>Destination : {item["destination"]}<br/><button onClick={() => this.deleteNotificationChannel(item["id"])}>Supprimer</button></p></div>)}
+                <hr/>
+                <div>
+                    Nom : <input id="name" type="text"/><br/>
+                    Type : <input id="type" type="text"/><br/>
+                    Destination : <input id="destination" type="text"/><br/>
+                    <button onClick={() => this.addNotificationChannel()}>Créer</button>
+                </div>
             </div>
         );
     }
