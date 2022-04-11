132673ae52481c6a5b0fce0606d46e56dd00abd0
Thomas <thomas0bellon@gmail.com>
2022-04-11 15:03:25 +0200

    fl0pp4

--- src/main/java/me/guillaume/chuck_facts/application/SimpleEmailExampleController.java
+++ src/main/java/me/guillaume/chuck_facts/application/SimpleEmailExampleController.java
@@ -1,33 +1,70 @@
 package me.guillaume.chuck_facts.application;
 
+import me.guillaume.chuck_facts.infrastructure.persistence.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
 public class SimpleEmailExampleController {
 
     @Autowired
     public JavaMailSender emailSender;
 
-    @ResponseBody
-    @RequestMapping("/sendSimpleEmail")
-    public String sendSimpleEmail() {
+    @Autowired
+    UsersRepository usersService;
 
+    @Autowired
+    NotificationRepository notificationService;
+
+    @Autowired
+    NotificationChannelsRepository notificationChannelsService;
+
+    public void sendEmail(String email, String subject, String text){
         // Create a Simple MailMessage.
         SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setTo("tyazze@gmail.com");
-        message.setSubject("Test Simple Email");
-        message.setText("Hello, Im testing Simple Email");
+        message.setTo(email);
+        message.setSubject(subject);
+        message.setText(text);
 
         // Send Message!
         this.emailSender.send(message);
+    }
 
-        return "Email Sent!";
+    @GetMapping("/sendSimpleEmail")
+    public String sendSimpleEmail() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String currentDate =  dateFormat.format(new Date());
+
+        String ret = "ok";
+        List<Users> listUser = usersService.findAll();
+        for(Users temp: listUser){
+            List<NotificationChannels> listChannel = temp.getNotificationChannels();
+            List<Notification> listNotification = temp.getNotifications();
+
+            for(Notification tempNotif: listNotification){
+                for(NotificationChannels tempChannel : listChannel){
+                    if(tempNotif.getDate().equals(currentDate)){
+                        sendEmail(tempChannel.getDestination(), tempNotif.getName(), tempNotif.getDescription());
+                    }
+                }
+            }
+        }
+
+        return currentDate;
     }
 
 }
